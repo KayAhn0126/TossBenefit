8fe6b07673e3f23f47c888865977d85251c065bb
KayAhn0126 <dkseotn123@gmail.com>
2022-10-15 21:27:45 +0900

    refactor: Benefit 구조체와 MyPoint 구조체 구현

---
+++ TossBenefit/TossBenefit/Home/BenefitListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BenefitListViewController.swift
+//  TossBenefit
+//
+//  Created by Kay on 2022/10/15.
+//
+
+import UIKit
+
+class BenefitListViewController: UIViewController {
+
+    // - 사용자는 포인트를 볼 수 있다.
+    // - 사용자는 오늘의 혜택을 볼 수 있다.
+    // - 사용자는 나머지 혜택 리스트를 볼 수 있다. -> 여기까지 생각해보면 collection view 사용
+    //
+    // - 사용자는 포인트 셀을 눌렀을 때, 포인트 상세 뷰로 넘어간다. -> didSelectedItem func
+    // - 사용자는 혜택 관련 셀을 눌렀을 때, 혜택 상세 뷰로 넘어간다.
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TossBenefit/TossBenefit/Model/MyPoint.swift
@@ -0,0 +1,16 @@
+//
+//  MyPoint.swift
+//  TossBenefit
+//
+//  Created by Kay on 2022/10/15.
+//
+
+import Foundation
+
+struct MyPoint: Hashable {
+    var point: Int
+}
+
+extension MyPoint {
+    static let `default` = MyPoint(point: 0)
+}
