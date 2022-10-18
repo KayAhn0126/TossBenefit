4f22f8fae573b15f0e4f4af1a155a8a419c90e3a
KayAhn0126 <dkseotn123@gmail.com>
2022-10-19 00:24:58 +0900

    refactor: MyPointCell클래스내 configure 메서드 구현

--- TossBenefit/TossBenefit/Home/MyPointCell.swift
+++ TossBenefit/TossBenefit/Home/MyPointCell.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class MyPointCell: UICollectionViewCell {
     
+    @IBOutlet weak var myPointImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var currentPointLabel: UILabel!
+    
     func configure(item: MyPoint) {
-        
+        myPointImage.image = UIImage(named: "ic_point")
+        descriptionLabel.text = "내 포인트"
+        currentPointLabel.text = "\(item.point)원"
     }
 }
