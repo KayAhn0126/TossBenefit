fa96a75bdb5810a0e6bf38a209b2ba0a92dbf117
KayAhn0126 <dkseotn123@gmail.com>
2022-10-19 12:53:44 +0900

    refactor: 나머지 혜택 셀 구성

--- TossBenefit/TossBenefit/Home/OtherBenefitsCell.swift
+++ TossBenefit/TossBenefit/Home/OtherBenefitsCell.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class OtherBenefitsCell: UICollectionViewCell {
     
+    @IBOutlet weak var otherBenefitsImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var getPointLabel: UILabel!
+    
     func configure(item: OtherBenefits) {
-        
+        otherBenefitsImage.image = UIImage(named: item.imageName)
+        descriptionLabel.text = item.description
+        getPointLabel.text = item.title
     }
 }
