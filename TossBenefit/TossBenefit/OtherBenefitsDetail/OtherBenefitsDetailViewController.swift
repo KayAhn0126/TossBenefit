9fd74f4adb21ce700325f1fe0baa649acc58362d
KayAhn0126 <dkseotn123@gmail.com>
2022-10-19 19:57:44 +0900

    feat: OtherBenefitsDetail storyboard 및 view controller 생성

---
+++ TossBenefit/TossBenefit/OtherBenefitsDetail/OtherBenefitsDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OtherBenefitsDetailViewController.swift
+//  TossBenefit
+//
+//  Created by Kay on 2022/10/19.
+//
+
+import UIKit
+
+class OtherBenefitsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
