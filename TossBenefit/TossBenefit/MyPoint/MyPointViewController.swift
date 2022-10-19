43748cfb5115fd2cf1319bcde16c6974962222c6
KayAhn0126 <dkseotn123@gmail.com>
2022-10-19 16:08:35 +0900

    feat: MyPointDetail 화면 및 컨트롤러 구현

---
+++ TossBenefit/TossBenefit/MyPoint/MyPointViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyPointViewController.swift
+//  TossBenefit
+//
+//  Created by Kay on 2022/10/19.
+//
+
+import UIKit
+
+class MyPointViewController: UIViewController {
+
+    @IBOutlet weak var pointLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
